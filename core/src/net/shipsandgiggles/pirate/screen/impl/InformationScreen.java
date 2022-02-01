39ee8c8d80527cf10eda3c9b9572c61750a2fee9
parisyup <66366646+parisyup@users.noreply.github.com>
2022-02-01 17:26:20 +0000

    finalize information screen

--- core/src/net/shipsandgiggles/pirate/screen/impl/InformationScreen.java
+++ core/src/net/shipsandgiggles/pirate/screen/impl/InformationScreen.java
@@ -1,11 +1,16 @@
 package net.shipsandgiggles.pirate.screen.impl;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
-import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Timer;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
@@ -18,12 +23,15 @@ public class InformationScreen implements Screen {
     private Stage stage;
     private Table table;
 
+    private Timer.Task task;
+    public final Sprite background = new Sprite(new Texture(Gdx.files.internal("models/background.PNG")));;
+    private final SpriteBatch batch = new SpriteBatch();;
+
     @Override
     public void show() {
         this.table = new Table();
 
         this.table.setFillParent(true);
-        //this.table.setDebug(true);
 
         this.stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(this.stage);
@@ -47,6 +55,9 @@ public class InformationScreen implements Screen {
         Label singularShoot = new Label("Left-Click to singular shoot!", Configuration.SKIN, "big");
         singularShoot.setAlignment(Align.center);
 
+        Label spaceToSkip = new Label("Press the space bar to skip the information!", Configuration.SKIN);
+        spaceToSkip.setAlignment(Align.center);
+
         // Creates a uniform X/Y table.
         this.table.add(informationLabel);
         this.table.row();
@@ -66,17 +77,27 @@ public class InformationScreen implements Screen {
         this.table.row();
         this.table.add(singularShoot);
         this.table.row();
+        this.table.add(Configuration.SPACER_LABEL);
+        this.table.row();
+        this.table.add(Configuration.SPACER_LABEL);
+        this.table.row();
+        this.table.add(spaceToSkip);
 
-        Timer.schedule(new ChangeScreenTask(ScreenType.GAME), 5);
+        this.task = Timer.schedule(new ChangeScreenTask(ScreenType.GAME), 5);
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(165f / 255f, 220f / 255f, 236f / 255f, 1);
+        Gdx.gl.glClearColor(.98f, .91f, .761f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        background.draw(batch);
+        batch.end();
 
         this.stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
         this.stage.draw();
+
+        takeInput();
     }
 
     @Override
@@ -103,5 +124,11 @@ public class InformationScreen implements Screen {
     @Override
     public void dispose() {
         this.stage.dispose();
+    }
+
+    public void takeInput() {
+        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+            this.task.run();
+        }
     }
 }

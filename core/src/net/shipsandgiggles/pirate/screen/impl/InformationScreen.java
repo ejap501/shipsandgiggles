1582ca09aec6629feacd8850a6c46e9637b84df2
ejap501 <ejap501@york.ac.uk>
2022-04-26 19:37:21 +0100

    Updated information screen

--- core/src/net/shipsandgiggles/pirate/screen/impl/InformationScreen.java
+++ core/src/net/shipsandgiggles/pirate/screen/impl/InformationScreen.java
@@ -3,6 +3,7 @@ package net.shipsandgiggles.pirate.screen.impl;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Timer;
 import com.badlogic.gdx.graphics.GL20;
@@ -26,7 +27,8 @@ import net.shipsandgiggles.pirate.task.ChangeScreenTask;
  * Adds the ui for the information screen
  *
  * @author Team 23
- * @version 1.0
+ * @author Team 22 : Edward Poulter
+ * @version 2.0
  */
 public class InformationScreen implements Screen {
     // Main data store
@@ -46,11 +48,22 @@ public class InformationScreen implements Screen {
         Gdx.input.setInputProcessor(this.stage);
         this.stage.addActor(this.table);
 
+        // Creating all the textures
+        Texture goldCoinTexture = new Texture(Gdx.files.internal("models/gold_coin.png"));
+        Texture silverCoinTexture = new Texture(Gdx.files.internal("models/silver_coin.png"));
+        Texture copperCoinTexture = new Texture(Gdx.files.internal("models/copper_coin.png"));
+
+        Texture speedTexture = new Texture(Gdx.files.internal("models/speed_up.png"));
+        Texture invincibilityTexture = new Texture(Gdx.files.internal("models/invincibility.png"));
+        Texture damageIncreaseTexture = new Texture(Gdx.files.internal("models/damage_increase.png"));
+        Texture coinMultiplierTexture = new Texture(Gdx.files.internal("models/coin_multiplier.png"));
+        Texture pointMultiplierTexture = new Texture(Gdx.files.internal("models/point_multiplier.png"));
+
         // Creating all the labels
         Label informationLabel = new Label("INFORMATION!", Configuration.SKIN, "title");
         informationLabel.setAlignment(Align.center);
 
-        Label keysInformation = new Label("Use your arrow keys or WASD to move around.", Configuration.SKIN, "big");
+        Label keysInformation = new Label("Use your arrow keys or WASD to move around, ESC to save and exit.", Configuration.SKIN, "big");
         keysInformation.setAlignment(Align.center);
 
         Label shootingInformation = new Label("There are two methods of shooting, burst and singular.", Configuration.SKIN, "big");
@@ -65,9 +78,39 @@ public class InformationScreen implements Screen {
         Label singularShoot = new Label("Left-Click to singular shoot!", Configuration.SKIN, "big");
         singularShoot.setAlignment(Align.center);
 
+        Label warning = new Label("Be careful not to kill the ducks, you will anger long boi!", Configuration.SKIN, "title");
+        warning.setAlignment(Align.center);
+        warning.setColor(1, 0, 0, 1);
+
+        Label gold = new Label("Gold Coin", Configuration.SKIN);
+        gold.setAlignment(Align.left);
+
+        Label silver = new Label("Silver Coin", Configuration.SKIN);
+        silver.setAlignment(Align.center);
+
+        Label copper = new Label("Copper Coin", Configuration.SKIN);
+        copper.setAlignment(Align.center);
+
+        Label speed = new Label("Increased Speed", Configuration.SKIN);
+        speed.setAlignment(Align.center);
+
+        Label invincibility = new Label("Invincibility", Configuration.SKIN);
+        invincibility.setAlignment(Align.center);
+
+        Label increasedDamage = new Label("Increased Damage", Configuration.SKIN);
+        increasedDamage.setAlignment(Align.center);
+
+        Label coinMultiplier = new Label("Coin Multiplier", Configuration.SKIN);
+        coinMultiplier.setAlignment(Align.center);
+
+        Label pointMultiplier = new Label("Point Multiplier", Configuration.SKIN);
+        pointMultiplier.setAlignment(Align.center);
+
+
+
         Label collegeInfo1 = new Label("There are two ways to win the game!", Configuration.SKIN, "big");
         Label collegeInfo2 = new Label("taking down a college will give you 5 gold and 3 score a second and if you capture all of them you win!", Configuration.SKIN, "big");
-        Label collegeInfo3 = new Label("the other way is to destroy all of them which will give you an instant 500 gold and 250 score!", Configuration.SKIN, "big");
+        Label collegeInfo3 = new Label("the other way is to destroy all of them which will give you instant gold and points!", Configuration.SKIN, "big");
         Label collegeInfo4 = new Label("destroying all of them will result in a victory!", Configuration.SKIN, "big");
 
         Label spaceToSkip = new Label("Press the space bar to skip the information!", Configuration.SKIN);
@@ -96,6 +139,23 @@ public class InformationScreen implements Screen {
         this.table.row();
         this.table.add(Configuration.SPACER_LABEL);
         this.table.row();
+        this.table.add(gold);
+        this.table.row();
+        this.table.add(new Image(goldCoinTexture));
+        this.table.row();
+        this.table.add(silver);
+        this.table.row();
+        this.table.add(new Image(silverCoinTexture));
+        this.table.row();
+        this.table.add(copper);
+        this.table.row();
+        this.table.add(new Image(copperCoinTexture));
+        this.table.row();
+        this.table.row();
+        this.table.add(Configuration.SPACER_LABEL);
+        this.table.row();
+        this.table.add(Configuration.SPACER_LABEL);
+        this.table.row();
         this.table.row();
         this.table.add(collegeInfo1);
         this.table.row();
@@ -107,6 +167,35 @@ public class InformationScreen implements Screen {
         this.table.row();
         this.table.row();
         this.table.add(spaceToSkip);
+        this.table.row();
+        this.table.row();
+        this.table.add(Configuration.SPACER_LABEL);
+        this.table.row();
+        this.table.add(Configuration.SPACER_LABEL);
+        this.table.row();
+        this.table.add(speed);
+        this.table.row();
+        this.table.add(new Image(speedTexture));
+        this.table.row();
+        this.table.add(invincibility);
+        this.table.row();
+        this.table.add(new Image(invincibilityTexture));
+        this.table.row();
+        this.table.add(increasedDamage);
+        this.table.row();
+        this.table.add(new Image(damageIncreaseTexture));
+        this.table.row();
+        this.table.add(coinMultiplier);
+        this.table.row();
+        this.table.add(new Image(coinMultiplierTexture));
+        this.table.row();
+        this.table.add(pointMultiplier);
+        this.table.row();
+        this.table.add(new Image(pointMultiplierTexture));
+        this.table.row();
+        this.table.row();
+        this.table.add(warning);
+        this.table.row();
 
         this.task = Timer.schedule(new ChangeScreenTask(ScreenType.GAME), 40);
     }

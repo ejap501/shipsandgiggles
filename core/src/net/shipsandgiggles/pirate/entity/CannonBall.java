96ac325a9bc6019359e38695153b9c60cb670009
Ethan Alabaster <ethanalabaster19@gmail.com>
2022-05-02 21:11:48 +0100

    Another cannonball update for potential fix.

--- core/src/net/shipsandgiggles/pirate/entity/CannonBall.java
+++ core/src/net/shipsandgiggles/pirate/entity/CannonBall.java
@@ -33,7 +33,7 @@ public class CannonBall {
     public boolean setAngle = false;
     public float angle;
     public Sprite cannonBall;
-    public float speed = 1.1f;
+    public float speed = 3f;
     public float damageDelt = 50f;
     public boolean teleported = false;
     public float finalX = 0;
@@ -240,7 +240,7 @@ public class CannonBall {
         //this.body.applyForceToCenter(this.body.getWorldVector(new Vector2(0, 20f)), true);
 
         // Gets the direction the ball is going towards
-        Vector2 direction = new Vector2(this.body.getWorldPoint(new Vector2(0,this.cannonBall.getHeight())));
+        Vector2 direction = new Vector2(this.body.getWorldPoint(new Vector2(0,this.cannonBall.getHeight() / 4)));
         Vector2 position = this.body.getPosition();
 
         // Changes the direction and slightly teleports the ball so it can travel way faster
